635e62418aeba9afeb8dbe027014488194984e5e
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-07-01 08:31:45 -0400

    creating get post & delete methods

---
+++ routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const { createNewNote, deleteNote } = require('../../lib/notes');
+const notesArray = require('../../db/notes');
+
+// GET route to retrieve all notes
+router.get('/notes', (req, res) => {
+  res.json(notesArray);
+});
+
+// POST route to create a new note
+router.post('/notes', (req, res) => {
+  const newNote = {
+    id: notesArray.length.toString(),
+    ...req.body
+  };
+  
+  notesArray.push(newNote);
+  res.json(newNote);
+});
+
+// DELETE route to delete a note
+router.delete('/notes/:id', (req, res) => {
+  const { id } = req.params;
+  const index = notesArray.findIndex(note => note.id === id);
+  
+  if (index !== -1) {
+    const deletedNote = notesArray.splice(index, 1)[0];
+    res.json(deletedNote);
+  } else {
+    res.status(404).json({ error: 'Note not found' });
+  }
+});
+
+module.exports = router;
