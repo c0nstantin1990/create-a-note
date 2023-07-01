fb5fb872f10fcedf0f8dcd807ca101c5614e73f5
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-07-01 08:08:25 -0400

    adding routest to server.js

--- server.js
+++ server.js
@@ -2,30 +2,18 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = 3001;
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// API routes
-app.get("/api/notes", (req, res) => {
-  // Implement the logic to fetch notes from the database and send the response
-});
+app.use(express.static("public"));
 
-app.post("/api/notes", (req, res) => {
-  // Implement the logic to save a new note to the database and send the response
-});
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-app.delete("/api/notes/:id", (req, res) => {
-  // Implement the logic to delete a note from the database and send the response
-});
-
-// Serve the React application
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-// Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`API server now on port ${PORT}!`);
 });
