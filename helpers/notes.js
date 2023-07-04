caa9e42c13caff5ce13b3df38b6c363e9b561325
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-07-04 14:32:25 -0400

    fixing deployment issue

--- helpers/notes.js
+++ helpers/notes.js
@@ -1,28 +1,31 @@
-const fs = require("fs").promises;
+const fs = require("fs");
 const path = require("path");
 
 // Adds a new note to an array and saves it
-async function createNewNote(body, notesArray) {
+function createNewNote(body, notesArray) {
   const note = body;
   notesArray.push(note);
-  await writeNotesToFile(notesArray);
+  writeNotesToFile(notesArray);
   return note;
 }
 // Removes a note from an array and updates the file
-async function deleteNote(id, notesArray) {
-  const updatedNotesArray = notesArray.filter((el) => el.id != id);
-  await writeNotesToFile(updatedNotesArray);
-  return updatedNotesArray;
+function deleteNote(id, notes) {
+  const notesArray = notes.filter((el) => el.id != id);
+  reindexNotes(notesArray);
+  writeNotesToFile(notesArray);
+  return notesArray;
 }
 // Writes an array of notes to a JSON file
-async function writeNotesToFile(notesArray) {
-  try {
-    const filePath = path.join(__dirname, "../db/notes.json");
-    await fs.writeFile(filePath, JSON.stringify({ notesArray }, null, 2));
-  } catch (error) {
-    console.error("Error writing notes to file:", error);
-    throw error;
-  }
+function writeNotesToFile(notesArray) {
+  const filePath = path.join(__dirname, "../db/notes.json");
+  const data = JSON.stringify({ notesArray }, null, 2);
+  fs.writeFileSync(filePath, data);
+}
+// Updates note IDs based on their index position
+function reindexNotes(notesArray) {
+  notesArray.forEach((note, index) => {
+    note.id = index;
+  });
 }
 
 module.exports = {
